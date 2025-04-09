aa0afe78d7d561ce145b9e3dcdd7e249e07d27c8
Ion <127430807+IonGadarenco@users.noreply.github.com>
2025-04-09 18:21:00 +0300

    Create c# project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace classes_in_cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
